9f1365866351638acc33b4ec19568c0aa0bd0c1e
shinraqw <oliynik05vitaliy@gmail.com>
2023-05-10 21:49:12 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,31 @@
+let myImage = document.querySelector('img');
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function () {
+    setUserName();
+}
